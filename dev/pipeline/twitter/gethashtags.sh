1d754097ecc50e55d4149f4cbd99b194bd2c56c5
superposition <ericmanganaro@gmail.com>
2017-07-23 07:10:54 +0000

    Twitter to IFTTT Google Sheets Recipe to JSON

---
+++ dev/pipeline/twitter/gethashtags.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsjson 1hAT5hBv70fLAPwRCGRBf8DKSXpB68abyfGAfG-allZU dgb.json -b
+gsjson 1yBEvI-EUbM2xk7Ul9GE-ZDo81_WBAdraZw3mBDN29DM xrp.json -b
+gsjson 1hmB-Mx6J_NxTUUDI4yfYtTd2it92O_su6z9pEaAyyLQ eth.json -b
+
